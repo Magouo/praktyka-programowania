5d21337c94d81b90c2423a326618f56bc7fdf47e
Michał Lipiec <115108105+Magouo@users.noreply.github.com>
2025-05-20 23:29:34 +0200

    fix 4

--- utils.py
+++ utils.py
@@ -1,17 +1,17 @@
 """Podstawowe testy"""
 
-def add ( a: int , b: int) -> int:
+def add(a: int, b: int) -> int:
     """suma"""
     return a + b
 
-def subtract ( a: int , b: int) -> int:
+def subtract(a: int, b: int) -> int:
     """różnica"""
     return a - b
 
-def multiply ( a: int , b: int) -> int:
+def multiply(a: int, b: int) -> int:
     """mnożenie"""
     return a * b
 
-def divide ( a: int , b: int) -> float :
+def divide(a: int, b: int) -> float:
     """dzielenie"""
     return a / b

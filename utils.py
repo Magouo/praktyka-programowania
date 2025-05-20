52628165ef4a0664475b68a62681189de78c6cbf
Michał Lipiec <115108105+Magouo@users.noreply.github.com>
2025-05-20 23:14:38 +0200

    Fix: add docstrings to utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
+"""Podstawowe testy"""
+
 def add ( a: int , b: int) -> int:
+    """suma"""
     return a + b
+
 def subtract ( a: int , b: int) -> int:
+    """różnica"""
     return a - b
+
 def multiply ( a: int , b: int) -> int:
+    """mnożenie"""
     return a * b
+
 def divide ( a: int , b: int) -> float :
+    """dzielenie"""
     return a / b
